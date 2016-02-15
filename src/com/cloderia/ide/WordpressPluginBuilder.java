9499807ee49c30249b5a71fc0d975880d3508b39
Edward Banfa <ebanfa@gmail.com>
2016-02-15 10:04:20 +0300

    Shadow origin

--- src/com/cloderia/ide/WordpressPluginBuilder.java
+++ src/com/cloderia/ide/WordpressPluginBuilder.java
@@ -195,7 +195,7 @@ public class WordpressPluginBuilder extends ApplicationBuilder {
 					String targetEntityPostName = field.getDataType();
 					for(Entity item: cloneOfEntitiesInModule){
 						if(item.getPostName().equals(targetEntityPostName)) {
-							String fieldName = field.getName(); //+ UUID.randomUUID().toString();
+							String fieldName = field.getName() + UUID.randomUUID().toString();
 							//System.out.println("Adding child: " + entity.getName() + " to parent: " + item.getName());
 							if(item.getName().equals("Property")) {
 								
