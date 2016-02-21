c96cbaf3d33ceb78dd21b6199ae17c049f45c4d3
Edward Banfa <ebanfa@gmail.com>
2016-02-21 18:40:19 +0300

    Shadow origin

--- src/com/cloderia/ide/WordpressPluginBuilder.java
+++ src/com/cloderia/ide/WordpressPluginBuilder.java
@@ -328,6 +328,8 @@ public class WordpressPluginBuilder extends ApplicationBuilder {
 			} 
 			
 		}
+		this.generateArtifact(module, null, "views/wordpress/tenantagreement/create-entity-view-php.ftl" , includeViewOutputDir +  "TenantAgreementCreateView.php");
+		this.generateArtifact(module, null, "views/wordpress/create-multi-entity.ftl" , includeViewOutputDir +  "MultiEntityCreateView.php");
 		this.generateArtifact(module, null, "views/wordpress/view-controller-php.ftl" , includeViewOutputDir +  "ViewController.php");
 		this.generateArtifact(module, null, "views/wordpress/base-artifact-view.ftl" , includeViewOutputDir +  "ArtifactView.php");
 		this.generateArtifact(module, null, "views/wordpress/base-entity-view.ftl" , includeViewOutputDir +  "BaseEntityView.php");
@@ -394,6 +396,7 @@ public class WordpressPluginBuilder extends ApplicationBuilder {
 		this.generateArtifact(module, null, "html/wordpress/entity/single.ftl" , entityOutputDir + "single.php");
 		this.generateArtifact(module, null, "html/wordpress/entity/entity-form-fields.ftl" , entityOutputDir + "entity-form-fields.php");
 		this.generateArtifact(module, null, "html/wordpress/entity/related-field-modals.ftl" , entityOutputDir + "related-entity-field-modals.php");
+		this.generateArtifact(module, null, "html/wordpress/entity/multi-entity-create.ftl" , entityOutputDir + "multi-entity-create-form.php");
 
 		this.generateArtifact(module, null, "html/wordpress/entity/field-wrapper-end.ftl" , entityOutputDir + "field-wrapper-end.php");
 		this.generateArtifact(module, null, "html/wordpress/entity/field-wrapper-start.ftl" , entityOutputDir + "field-wrapper-start.php");
